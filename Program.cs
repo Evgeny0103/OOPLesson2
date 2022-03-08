85a8907a0a523d95f01f392a42b954f8da275557
Evgeny0103 <38032854+Evgeny0103@users.noreply.github.com>
2022-03-08 12:30:11 +0600

    Step 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
